16c275d07c8f015b30dd4b238dc97d9b87aa4abc
Phil Selleh <pselleh@gmail.com>
2026-04-25 00:56:40 +0000

    Add working views and fixes

--- src/catalog_extensions/apps.py
+++ src/catalog_extensions/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class CatalogExtensionsConfig(AppConfig):
     name = "catalog_extensions"
-
-    def ready(self):
-        import catalog_extensions.signals  # noqa
