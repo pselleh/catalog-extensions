1f19d4f059618cb6013ff077b9c149548a8fdfbb
Phil Selleh <pselleh@gmail.com>
2026-05-01 00:03:42 +0000

    Move URL wiring to AppConfig.ready()

--- src/catalog_extensions/apps.py
+++ src/catalog_extensions/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class CatalogExtensionsConfig(AppConfig):
     name = "catalog_extensions"
+
+    def ready(self):
+        from django.urls import include, path
+        import course_discovery.urls
+
+        # Prevent duplicate injection on reload
+        if not any("catalog_extensions.urls" in str(p) for p in course_discovery.urls.urlpatterns):
+            course_discovery.urls.urlpatterns += [
+                path("", include("catalog_extensions.urls")),
+            ]
