c993abd88e29839d735d24635275bf361b9be917
Phil Selleh <pselleh@gmail.com>
2026-06-30 15:00:20 +0000

    Implement CBA Discovery catalog extension framework

--- catalog_extensions/urls.py
+++ catalog_extensions/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from catalog_extensions.api.views import UnifiedCatalogView
+from django.urls import include, path
 
 urlpatterns = [
-    path("unified-catalog/", UnifiedCatalogView.as_view()),
+    path("api/cba/v1/courses/", include("catalog_extensions.urls.courses")),
+    path("api/cba/v1/programs/", include("catalog_extensions.urls.programs")),
+    path("api/cba/v1/organizations/", include("catalog_extensions.urls.organizations")),
+    path("api/cba/v1/subjects/", include("catalog_extensions.urls.subjects")),
+    path("api/cba/v1/search/", include("catalog_extensions.urls.search")),
+    path("api/cba/v1/media/", include("catalog_extensions.urls.media")),
+    path("api/cba/v1/homepage/", include("catalog_extensions.urls.homepage")),
 ]
