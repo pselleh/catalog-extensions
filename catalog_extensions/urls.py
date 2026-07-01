2a0d316c59a82895c364a03d9c072572f1bbd30c
Phil Selleh <pselleh@gmail.com>
2026-07-01 13:48:11 +0000

    Simplify catalog extension URL prefixing

--- catalog_extensions/urls.py
+++ catalog_extensions/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("api/cba/v1/courses/", include("catalog_extensions.urls.courses")),
-    path("api/cba/v1/programs/", include("catalog_extensions.urls.programs")),
-    path("api/cba/v1/organizations/", include("catalog_extensions.urls.organizations")),
-    path("api/cba/v1/subjects/", include("catalog_extensions.urls.subjects")),
-    path("api/cba/v1/search/", include("catalog_extensions.urls.search")),
-    path("api/cba/v1/media/", include("catalog_extensions.urls.media")),
-    path("api/cba/v1/homepage/", include("catalog_extensions.urls.homepage")),
+    path("courses/", include("catalog_extensions.urls.courses")),
+    path("programs/", include("catalog_extensions.urls.programs")),
+    path("organizations/", include("catalog_extensions.urls.organizations")),
+    path("subjects/", include("catalog_extensions.urls.subjects")),
+    path("search/", include("catalog_extensions.urls.search")),
+    path("media/", include("catalog_extensions.urls.media")),
+    path("homepage/", include("catalog_extensions.urls.homepage")),
 ]
